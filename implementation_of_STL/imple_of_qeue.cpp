8f9ef6093e7046459530bfb5e262117e38e59b54
nazmulfahim000 <nazmulfahim000@gmail.com>
2025-03-11 00:13:16 +0600

    imple..

--- implementation_of_STL/imple_of_qeue.cpp
+++ implementation_of_STL/imple_of_qeue.cpp
@@ -1,31 +1,59 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-class qu{
+class queue{
     private:
-        int topp;
-        int tail;
-        int count;
-        int a[100];
+        int head,tail,count;
+        int arr[100];
     public:
-        qu(){
-            topp=0;
+        queue(){
+            head=0;
             tail=0;
             count=0;
         }
-        void enqueue(int x){
-            a[tail]=x;
+
+        void push(int num){
+            arr[head]=num;
+            head++;
+            count++;
+
+        }
+
+        void pop(){
             tail++;
-            count++;    
-        }
-        void dqueue_from_front(){
-            topp--;
-        }
-        
+            count--;
 
+        }
 
+        void print(){
+            if(head>=tail){
+                for(int i=tail;i<head;i++){
+                    cout<<arr[i]<<" ";
+                }
+            }
+            else{
+                cout<<"your queue is empty";
+            }
+        }
+
+        void front(){
+            cout<<arr[tail]<<endl;
+            
+        }
 };
 int main(){
-    qu q;
-    q.enqueue(1);q.enqueue(2);q.enqueue(3);q.enqueue(4);q.enqueue(5);
-    q.dqueue_from_front();
+    queue q;
+    q.push(10);
+    q.push(120);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+
+
+    q.pop();
+
+    q.print();
+    cout<<endl;
+    
+    q.front();
+
 }
