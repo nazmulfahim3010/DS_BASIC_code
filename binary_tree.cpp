7350c097c531b5b60a7069d0a09baeed0124b53b
nazmulfahim000 <nazmulfahim000@gmail.com>
2025-03-10 21:14:41 +0600

    tree

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -1,59 +1,92 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct Node{
+struct node{
     int data;
-    Node*left;
-    Node*right;
-    Node(int item){
+    node*left;
+    node*right;
+    node(int item){
         data=item;
         left=NULL;
         right=NULL;
     }
-
 };
-
 class binary_tree{
     private:
-        Node*root;
+    node*root;
+    
     public:
     binary_tree(){
         root=NULL;
     }
-        void add_on_left(int item){
-            Node*new_node=new Node(item);
-            Node*temp=root;
+    node* create_node(int num){
+        node*new_node=new node(num);
+        return new_node;
 
-            if(root==NULL){
-                root=new_node;
-                return;
-            }
+    }
+    void add_left(node*parent,node*child){
+        parent->left=child;
+    }
+    void add_right(node*parent,node*child){
+        parent->right=child;
+    }
 
-            while(temp->left!=NULL){
-                temp=temp->left;
-            }
-            temp->left=new_node;
+    void pre_order(node*root){
+        if(root==NULL){
+            return;
         }
 
-        void add_on_right(int item){
-            Node*new_node=new Node(item);
-            Node*temp=root;
+        cout<<root->data<<" ";
+        pre_order(root->left);
+        pre_order(root->right);
+        
+    }
 
-            if(root==NULL){
-                root=new_node;
-                return;
-            }
-
-            while(temp->right!=NULL){
-                temp=temp->right;
-            }
-            temp->right=new_node;
+    void in_order(node*root){
+        if(root==NULL){
+            return;
         }
+
+        in_order(root->left);
+        cout<<root->data<<" ";
+        in_order(root->right);
+    }
+
+    void post_order(node*root){
+        if(root==NULL){
+            return;
+        }
+
+        post_order(root->left);
+        post_order(root->right);
+        cout<<root->data<<" ";
+    }
+
+
+
 };
-
 int main(){
     binary_tree tree;
-    tree.add_on_left(1);
-    tree.add_on_left(2);
-    tree.add_on_right(3);
-    tree.add_on_right(4);
+    node*root=tree.create_node(1);
+    node*one=tree.create_node(2);
+    node*two=tree.create_node(3);
+    node*three=tree.create_node(4);
+    node*four=tree.create_node(5);
+    node*five=tree.create_node(6);
+
+    tree.add_left(root,one);
+    tree.add_right(root,two);
+    tree.add_left(one,three);
+    tree.add_right(one,four);
+    tree.add_left(two,five);
+
+    cout<<"Pre-order : ";
+    tree.pre_order(root);
+    cout<<endl;
+    cout<<"In-order : ";
+    tree.in_order(root);
+    cout<<endl;
+    cout<<"post-order : ";
+    tree.post_order(root);
+
+
 }
