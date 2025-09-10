65053e6f554438ceae32f2d7daa063c7cb10462c
nazmulfahim000 <nazmulfahim000@gmail.com>
2025-09-10 22:29:39 +0600

    selecttion py

---
+++ ARRAY py/selection_sort.py
@@ -0,0 +1,18 @@
+list=[]
+n=int(input("enter number of number :"))
+for _ in range(n):
+    x=int(input())
+    list.append(x)
+minimum_ind=0
+for i in range(n-1):
+    minimum_ind=i
+    for j in range(i+1,n):
+        if list[minimum_ind]>list[j]:
+            minimum_ind=j
+    
+    list[minimum_ind],list[i]=list[i],list[minimum_ind]
+
+print(*list)
+
+    
+
